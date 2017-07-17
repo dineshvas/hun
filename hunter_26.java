837517ed68b9eeca0308e14e90ff2c16c704ad92
dineshvas <dineshkumar9569@gmail.com>
2017-07-17 10:59:06 +0530

    Add files via upload

---
+++ hunter_26.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class hunter_26 {
+	public static void main(String[] args){
+		ArrayList<String> l=new ArrayList();
+		l.add("hai");
+		l.add("hello");
+		l.add("Bye");
+		System.out.println(l);
+		Collections.reverse(l);
+		System.out.println(l);
+		
+	}
+
+}
