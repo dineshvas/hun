e5af05d59c7f594ee630c5b58024d6e7a6e429f2
dineshvas <dineshkumar9569@gmail.com>
2017-07-17 13:55:47 +0530

    Add files via upload

---
+++ seed.java
@@ -0,0 +1,34 @@
+package pro;
+import java.util.Scanner;
+public class seed {
+	public static void main(String[] args){
+			int number;
+			Scanner scr=new Scanner(System.in);
+			number=scr.nextInt();
+			int count=0;
+			int[] arr=new int[number];
+			for(int i=10;i<number/2;i++){
+				int m=i;
+				int sum=1;
+				while(m>0){
+					int k=0;
+					
+					arr[k]=m%10;
+					m=m/10;
+					sum*=arr[k];
+					
+				}
+			
+				if(i*sum==number){
+					System.out.println("SEED IS "+i);
+				count++;	
+				}
+				
+			}
+			if(count==0)
+				System.out.println("There is no seed");
+			scr.close();
+
+	}
+
+}
