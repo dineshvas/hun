35db5bc2b1052b2ed5403d2d69d663c85ba6ce47
dineshvas <dineshkumar9569@gmail.com>
2017-07-19 15:21:18 +0530

    Add files via upload

---
+++ Hun51.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.Scanner;
+public class Hun51 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter nuumbers:");
+		String a=sc.nextLine();
+		String b=sc.nextLine();
+		int c=Integer.parseInt(a);
+		int d=Integer.parseInt(b);
+		int m=c*d;
+		System.out.println(String.valueOf(m));
+				
+	}
+
+}
