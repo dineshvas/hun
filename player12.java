f3277d161c5e5f1f480a832a78f715ea2e52e7ba
dineshvas <dineshkumar9569@gmail.com>
2017-07-19 15:58:48 +0530

    Add files via upload

---
+++ player12.java
@@ -0,0 +1,26 @@
+package guvi;
+import java.util.Scanner;
+public class player12 {
+public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the array size");
+			int n=sc.nextInt();
+			int a[]=new int[n];
+			for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();		
+		}
+				System.out.println("Enter the number:");
+				int d=sc.nextInt();
+				for(int i=0;i<n;i++){
+					if(a[i]==d){
+						for(int x=i;x<n-1;x++){
+							System.out.println(a[x+1]);
+						}
+						for(i=0;i!=d;i++){
+							System.out.println(a[i]);
+		}							
+	}
+	}
+
+}
+}
